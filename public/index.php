bffcf1990deec9afc23ddde01d76e755902f35b4
PaulKl10 <paul.klein43@gmail.com>
2023-08-09 14:54:13 +0200

    autorisation d'accès au port 4200

--- public/index.php
+++ public/index.php
@@ -1,8 +1,13 @@
 <?php
 
+header('Access-Control-Allow-Origin: http://localhost:4200'); // Remplacez avec l'URL de votre application front-end
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Credentials: true'); // Activez ceci si vous avez besoin d'utiliser des cookies ou des identifiants d'authentification
+
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
